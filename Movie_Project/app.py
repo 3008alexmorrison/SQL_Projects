385bfb56a578033435d836e863234dfda4ea5cfc
Alex <3008alexmorrison@gmail.com>
2024-03-05 12:17:46 -0800

    Beginning of Migration from SQLite3 to Postgres/Psycopg2-binary

--- Movie_Project/app.py
+++ Movie_Project/app.py
@@ -1,12 +1,98 @@
-import psycopg2
+import database
+import datetime
 
-url = "postgres://gdebcbmi:esLXBF9L1qHAhLDJhAqGohIGMen1SKEb@bubble.db.elephantsql.com/gdebcbmi"
-connection = psycopg2.connect(url)
-cursor = connection.cursor()
+menu = """Please select one of the following options:
+1) Add new movie.
+2) View upcoming movies.
+3) View all movies
+4) Watch a movie
+5) View watched movies.
+6) Add new user
+7) Search movies
+8) Exit.
 
-cursor.execute("SELECT * FROM users;")
-first_user = cursor.fetchone()
+Your selection: """
 
-print(first_user)
+welcome = "Welcome to the watchlist app!"
 
-connection.close()
+
+print(welcome)
+database.create_tables()
+
+#functions for menu options
+def prompt_add_movie():
+    #prompt for title and release date
+    title = input("Movie title: ")
+    release_date = input("Release date (dd-mm-YYYY): ")
+    #parse release date into timestamp for later use
+    parsed_date = datetime.datetime.strptime(release_date, "%d-%m-%Y")
+    timestamp = parsed_date.timestamp()
+    #add movie to database
+    database.add_movie(title, timestamp)
+
+def timestamp_to_date(timestamp):
+    #unparse timestamp into default format date
+    date = datetime.datetime.fromtimestamp(timestamp)
+    #format date into more readable format and convert to string
+    formatted_date = date.strftime("%B %d, %Y")
+    return formatted_date
+
+def print_movie_list(heading, movies):
+    #header
+    print(f"-- {heading} Movies --")
+    #iterate through database to get specified movies
+    for _id, title, release_date in movies:
+        #format timestamp to readable date
+        date_of_release = timestamp_to_date(release_date)
+        #print movie list
+        print(f"{_id}: {title} | Released On: {date_of_release}")
+    print("--- \n")
+
+def prompt_add_user():
+    username = input("Username: ")
+    database.add_user(username)
+
+
+def prompt_watch_movie():
+    #Who are you and what was the movie that you watched?
+    username = input("Please enter your username: ")
+    movie_id = input("Movie ID: ")
+    #mark watched
+    database.watch_movie(username, movie_id)
+
+def prompt_show_watched_movies():
+    username = input("Please enter your username: ")
+    movies = database.get_watched_movies(username)
+    if movies:
+        print_movie_list("Watched", movies)
+    else:
+        print("This user hasn't watched any movies yet!")
+
+def prompt_search_movies():
+    search_term = input("Enter movie title or partial movie title: ")
+    movies = database.search_movies(search_term)
+    if movies:
+        print_movie_list("Movies Found:", movies)
+    else:
+        print("No movies found for that search term!")
+
+#Menu while loop
+while (user_input := input(menu)) != "8":
+    if user_input == "1":
+        prompt_add_movie()
+    elif user_input == "2":
+        movies = database.get_movies(True)
+        print_movie_list("Upcoming", movies)
+    elif user_input == "3":
+        movies = database.get_movies()
+        print_movie_list("All", movies)
+    elif user_input == "4":
+        prompt_watch_movie()
+    elif user_input == "5":
+        prompt_show_watched_movies()
+    elif user_input == "6":
+        prompt_add_user()
+    elif user_input == "7":
+        prompt_search_movies()
+    else:
+        print("Invalid input, please try again!")
---
+++ Movie_Project/database.py
@@ -0,0 +1,98 @@
+import psycopg2
+import datetime
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+#objects that contain sql queries for functions
+CREATE_MOVIES_TABLE = """ CREATE TABLE IF NOT EXISTS movies (
+    id INTEGER PRIMARY KEY,
+    title TEXT,
+    release_timestamp REAL
+);"""
+
+CREATE_WATCHED_TABLE = """ CREATE TABLE IF NOT EXISTS watched (
+    user_username TEXT,
+    movie_id INTEGER,
+    FOREIGN KEY(user_username) REFERENCES users(username),
+    FOREIGN KEY(movie_id) REFERENCES movies(id)
+);"""
+
+CREATE_USERS_TABLE = """ CREATE TABLE IF NOT EXISTS users (
+    username TEXT PRIMARY KEY
+);"""
+
+INSERT_MOVIES = "INSERT INTO movies (title, release_timestamp) VALUES (?, ?);"
+
+INSERT_USER = "INSERT INTO users (username) VALUES (?);"
+
+SELECT_ALL_MOVIES = "SELECT * FROM movies;"
+
+SELECT_UPCOMING_MOVIES = "SELECT * FROM movies WHERE release_timestamp > ?;"
+
+SELECT_WATCHED_MOVIES = """ SELECT movies.* FROM movies
+JOIN watched ON movies.id = watched.movie_id
+JOIN users on users.username = watched.user_username
+WHERE users.username = ?;"""
+
+INSERT_WATCHED_MOVIE = "INSERT INTO watched (user_username, movie_id) VALUES (?, ?);"
+
+DELETE_MOVIE = "DELETE FROM movies WHERE title = ?;"
+
+SEARCH_MOVIE = "SELECT * FROM movies WHERE title LIKE ?;"
+
+CREATE_RELEASE_INDEX = "CREATE INDEX IF NOT EXISTS idx_movies_release ON movies (release_timestamp);"
+
+#connect to database
+connection = psycopg2.connect(os.environ['DATABASE_URL'])
+
+def create_tables():
+    with connection:
+        #Create tables if they do not exist in database
+        connection.execute(CREATE_MOVIES_TABLE)
+        connection.execute(CREATE_USERS_TABLE)
+        connection.execute(CREATE_WATCHED_TABLE)
+        connection.execute(CREATE_RELEASE_INDEX)
+
+def add_user(username):
+    with connection:
+        connection.execute(INSERT_USER, (username,))
+
+def add_movie(title, release_timestamp):
+    with connection:
+        #add movie to database
+        connection.execute(INSERT_MOVIES, (title, release_timestamp))
+
+def get_movies(upcoming=False):
+    with connection:
+        cursor = connection.cursor()
+        #check if upcoming
+        if upcoming:
+            #get today's time stamp
+            today_timestamp = datetime.datetime.today().timestamp()
+            #select movies that have a greater timestamp than today
+            cursor.execute(SELECT_UPCOMING_MOVIES, (today_timestamp,))
+        else:
+            #select all movies from database
+            cursor.execute(SELECT_ALL_MOVIES)
+        #return selected movies
+        return cursor.fetchall()
+    
+def search_movies(search_term):
+    with connection:
+        cursor = connection.cursor()
+        cursor.execute(SEARCH_MOVIE, (f"%{search_term}%",))
+        return cursor.fetchall()
+
+def watch_movie(username, movie_id):
+        with connection:
+            connection.execute(INSERT_WATCHED_MOVIE, (username, movie_id))
+
+def get_watched_movies(username):
+    with connection:
+        cursor = connection.cursor()
+        #select all movies with watched = 1
+        cursor.execute(SELECT_WATCHED_MOVIES, (username,))
+        #return selected movies 
+        return cursor.fetchall()
